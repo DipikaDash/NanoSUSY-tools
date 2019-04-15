aab02050078afae2dd2a4fdf71b9be249d830ccb
Dipika Dash <dipika.dash@cern.ch>
2019-04-15 18:48:09 -0500

    Update qcdskimmingfile.py

--- python/modules/qcdskimmingfile.py
+++ python/modules/qcdskimmingfile.py
@@ -21,14 +21,14 @@ class qcdskimmingfile(Module):
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        self.out.branch("met",  "F");
-        self.out.branch("met_phi", "F");
-	self.out.branch("jet_pt", "F");
-	self.out.branch("genjet_pt", "F");
-	# self.out.branch("dphij1met", "F");
-	# self.out.branch("dphij2met", "F");
-	# self.out.branch("dphij3met", "F");
-	# self.out.branch("dphij4met", "F");
+        self.out = wrappedOutputTree
+        self.out.branch("dphij1met", "F")
+        self.out.branch("dphij2met", "F")
+        self.out.branch("dphij3met", "F")
+        self.out.branch("dphij4met", "F")
+        self.out.branch("Pass_HEM20", "O")
+        self.out.branch("Pass_HEM30", "O")
+
 
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
@@ -42,20 +42,19 @@ class qcdskimmingfile(Module):
 	jets      = Collection(event, "Jet")
 	genjets   = Collection(event, "GenJet")
 	met       = Object(event,     self.metBranchName)
-
-
-	# njets = 0	
-	# for j in jets :
-	# 	njets += 1
-	# 	#dphij1met_ = deltaPhi(j, met)
-	# 	for gj in genjets :
-	# 		origRes_ = self.jetResFunction(j, gj)
-
-	# if njets > 0 : self.out.fillBranch("dphij1met", deltaPhi(jets[0], met))
-	# if njets > 1 : self.out.fillBranch("dphij2met", deltaPhi(jets[1], met))
-	# if njets > 2 : self.out.fillBranch("dphij3met", deltaPhi(jets[2], met))
-	# if njets > 3 : self.out.fillBranch("dphij4met", deltaPhi(jets[3], met))
-	#self.out.fillBranch("origRes", origRes_)
-        self.out.fillBranch("met", met.pt)
-        self.out.fillBranch("met_phi", met.phi)
+	njets = len(jets)
+        if met.pt < 200: return True
+        if njets < 2: return True
+        for i in jets:
+            if math.fabs(i.eta) > 4.7 or i.pt < 20: return True
+            else:
+                PassHEM20 = (i.eta > -3.2) & (i.eta < -1.2) & (i.phi > -1.77) & (i.phi<-0.67)
+                PassHEM30 = (i.pt > 30) & (i.eta > -3.2) & (i.eta < -1.2) & (i.phi > -1.77) & (i.phi<-0.67)
+                if njets > 0 : self.out.fillBranch("dphij1met", deltaPhi(jets[0], met))
+                if njets > 1 : self.out.fillBranch("dphij2met", deltaPhi(jets[1], met))
+                if njets > 2 : self.out.fillBranch("dphij3met", deltaPhi(jets[2], met))
+                if njets > 3 : self.out.fillBranch("dphij4met", deltaPhi(jets[3], met))
+                self.out.fillBranch("Pass_HEM20", PassHEM20)
+                self.out.fillBranch("Pass_HEM30", PassHEM30)
         return True
+
